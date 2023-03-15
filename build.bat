bad165d563d70fb9d20133c6d46604d6ababf3f4
seb <seb.degraff@gmail.com>
2023-03-15 10:32:46 +0100

    cleanup for windows

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-clang allocators_demo.cpp sokol_time.cpp           ^
+clang linear_allocator_demo.cpp sokol_time.cpp           ^
     -o program.exe                                 ^
     -g                                             ^
     -O0                                            ^
@@ -10,3 +10,9 @@ clang allocators_demo.cpp sokol_time.cpp           ^
     -lmsvcrt -lOpenGL32 -lGdi32 -lWinMM -lkernel32 -lshell32 -lUser32 -Xlinker /NODEFAULTLIB:libcmt
 REM Wondering how I knew what libraries to link with? Here you go -> https://stackoverflow.com/questions/72208683/what-standard-libraries-do-i-need-to-link-to-use-raylib-with-windows-and-clang
 REM I wish raylib had better documentation for this
+
+REM Stop on error
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Launch the program.exe
+program.exe
